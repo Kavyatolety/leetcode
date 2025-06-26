12139ac2354c488cef8c250d9d82edfdb8100dab
Kavya Tolety <69753724+Kavyatolety@users.noreply.github.com>
2025-06-26 00:20:51 -0400

    Time: 292 ms (68.16%), Space: 0B (100%) - LeetHub

---
+++ 3767-find-students-who-improved/3767-find-students-who-improved.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+WITH CTE AS (
+    SELECT
+        s.student_id,
+        s.subject,
+        FIRST_VALUE(score) OVER (
+            PARTITION BY student_id, subject
+            ORDER BY exam_date
+        ) AS first_score,
+        FIRST_VALUE(score) OVER (
+            PARTITION BY student_id, subject
+            ORDER BY exam_date DESC
+        ) AS latest_score
+    FROM scores s
+)
+SELECT *
+FROM cte
+GROUP BY student_id, subject
+HAVING first_score < latest_score;
